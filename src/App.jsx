9b586658bcf9273226f8739c50a49b9cdd80ed5c
olivier-defrance <238a007@gmail.com>
2025-11-22 22:18:38 +0100

    Ajout graphique 91

--- src/App.jsx
+++ src/App.jsx
@@ -250,7 +250,7 @@ setBestPerformance(
     <div>
       <h1>Simulateur IntradayGraf 2026</h1>
       <h2>
-        Basé sur l'historique des résultats du robot en backtest (2017 → 10/11/2025)
+        Basé sur les résultats du robot en backtest sur la période 2017 → 10/11/2025
       </h2>
     </div>
 
